297f01ff7ebd96f9d51a59e3ee8532976ae000eb
Грабчук Михаил Петрович <mgrabchuk@sv-tech.local>
2024-09-11 19:51:14 +0300

    feat: Event & Booking models created.

---
+++ bookmanager/book/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    total_places = models.IntegerField()
+    booked_places = models.IntegerField(default=0)
+    free_places = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Booking(models.Model):
+    user = models.CharField(max_length=100)
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    counter_of_places = Events.objects.all().count()
+
+
+# Create your models here.
